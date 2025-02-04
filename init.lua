e3150531ede113ef75c4686fb91b3726b508c1d0
LitFill <marrazzy54@gmail.com>
2025-02-04 21:16:54 +0700

    feat(mini): init

---
+++ init.lua
@@ -0,0 +1,155 @@
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path    = path_package .. 'pack/deps/start/mini.nvim'
+
+if not vim.loop.fs_stat(mini_path) then
+	vim.cmd('echo "Installing `mini.nvim`" | redraw')
+	local clone_cmd = {
+		'git', 'clone', '--filter=blob:none',
+		'https://github.com/echasnovski/mini.nvim', mini_path
+	}
+	vim.fn.system(clone_cmd)
+	vim.cmd('packadd mini.nvim | helptags ALL')
+	vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+require('mini.deps').setup({ path = { package = path_package } })
+
+local enabled_modules = {
+	'git',
+	'diff',
+	'icons',
+	'statusline',
+	'starter',
+	'indentscope',
+	'files',
+	'comment',
+	'completion',
+}
+
+for _, module in ipairs(enabled_modules) do
+	local mod = 'mini.' .. module
+	require(mod).setup()
+end
+
+require("options")
+require("keymaps")
+
+---@type function(plugin: { source: string })
+local add   = MiniDeps.add
+---@type function(function())
+local now   = MiniDeps.now
+-- local later = MiniDeps.later
+
+add({
+	source  = 'neovim/nvim-lspconfig',
+	depends = { 'williamboman/mason.nvim' },
+	name    = 'lspconfig',
+})
+
+require('mason').setup()
+local lsp = require('lspconfig')
+lsp.lua_ls.setup({})
+lsp.hls.setup({})
+
+add({
+	source   = 'nvim-treesitter/nvim-treesitter',
+	checkout = 'master',
+	monitor  = 'main',
+	hooks    = { post_checkout = function() vim.cmd('TSUpdate') end },
+})
+
+require('nvim-treesitter.configs').setup({
+	ensure_installed = { 'lua', 'vimdoc' },
+	highlight = { enable = true },
+})
+
+add {
+	source = "NeogitOrg/neogit",
+	depends = {
+		"nvim-lua/plenary.nvim", -- required
+		-- "sindrets/diffview.nvim", -- optional - Diff integration
+		-- "nvim-telescope/telescope.nvim", -- optional
+	},
+	config = true,
+}
+
+add({
+	source = "nuvic/flexoki-nvim",
+	name   = "flexoki",
+})
+
+require("flexoki").setup({
+	variant = "auto", -- auto, moon, or dawn
+	dark_variant = true,
+	dim_inactive_windows = false,
+	extend_background_behind_borders = true,
+
+	enable = {
+		terminal = true,
+	},
+
+	styles = {
+		bold = true,
+		italic = false,
+	},
+
+	groups = {
+		border = "muted",
+		link = "purple_two",
+		panel = "surface",
+
+		error = "red_one",
+		hint = "purple_one",
+		info = "cyan_one",
+		ok = "green_one",
+		warn = "orange_one",
+		note = "blue_one",
+		todo = "magenta_one",
+
+		git_add = "green_one",
+		git_change = "yellow_one",
+		git_delete = "red_one",
+		git_dirty = "yellow_one",
+		git_ignore = "muted",
+		git_merge = "purple_one",
+		git_rename = "blue_one",
+		git_stage = "purple_one",
+		git_text = "magenta_one",
+		git_untracked = "subtle",
+
+		h1 = "purple_two",
+		h2 = "cyan_two",
+		h3 = "magenta_two",
+		h4 = "orange_two",
+		h5 = "blue_two",
+		h6 = "cyan_two",
+	},
+
+	palette = {
+		-- Override the builtin palette per variant
+		-- moon = {
+		--	 base = '#100f0f',
+		--	 overlay = '#1c1b1a',
+		-- },
+	},
+
+	highlight_groups = {
+		Comment = { fg = "subtle", italic = true },
+		VertSplit = { fg = "muted", bg = "muted" },
+	},
+
+	-- before_highlight = function(group, highlight, palette)
+	before_highlight = function(_, _, _)
+		-- Disable all undercurls
+		-- if highlight.undercurl then
+		--	 highlight.undercurl = false
+		-- end
+		--
+		-- Change palette colour
+		-- if highlight.fg == palette.blue_two then
+		--	 highlight.fg = palette.cyan_two
+		-- end
+	end,
+})
+
+vim.cmd("colorscheme flexoki")
---
+++ keymaps.lua
@@ -0,0 +1,75 @@
+local set = vim.keymap.set
+
+---add desc to opts for set
+---@param opts vim.keymap.set.Opts
+---@param desc string
+---@return vim.keymap.set.Opts
+local d = function(opts, desc)
+    opts.desc = desc
+    return opts
+end
+
+-- convenient
+set({ "i", "t" }, "jk", "<Esc>", { nowait = true, desc = "exit to normal" })
+set("n", "<leader>qw", "<cmd>wq<CR>", { desc = "save and quit" })
+
+-- using makefile
+set("n", "<leader>mr", "<cmd>term make run<CR>", { desc = "(m)ake (r)un" })
+set("n", "<leader>mb", "<cmd>term make build<CR>", { desc = "(m)ake (b)uild" })
+set("n", "<leader>mqb", "<cmd>term make build<CR>a<CR>", { desc = "quick build" })
+
+set("n", "<leader>pv", vim.cmd.Ex, { desc = "preview pwd" })
+
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
+
+set("n", "J", "mzJ`z")
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+set("n", "n", "nzz")
+set("n", "N", "Nzz")
+
+set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- set("n", "<C-k>"    , "<cmd>cnext<CR>zz")
+-- set("n", "<C-j>"    , "<cmd>cprev<CR>zz")
+-- set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+set(
+    "n",
+    "<leader>rw",
+    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = "(r)eplace (w)ord" }
+)
+
+set("n", "-", "<cmd>lua MiniFiles.open()<CR>", { desc = "Open parent dir" })
+
+-- set("n", "<leader>utr", "<cmd>TransparentToggle<CR>", { desc = "Toggle transparent background" })
+
+set("t", "<C-q>", "<C-\\><C-n>", { nowait = true })
+set(
+	"n",
+	"<leader>ot",
+	"<cmd>term<CR><cmd>set nonumber norelativenumber<CR>a",
+	{ desc = "(o)pen (t)erminal" }
+)
+
+-- NeoGit
+set("n", "<leader>gn", "<cmd>Neogit<CR>", { desc = "open NeoGit" })
+
+-- Typst Preview
+-- set("n", "<leader>tp", "<cmd>TypstPreviewToggle<CR>", { desc = "Toggle Typst Preview in browser" })
+
+-- Idris2 lsp
+-- local bufopts = { noremap = true }
+-- local cmd = [[:lua require("idris2.code_action")]]
+-- set("n", "<leader>iev", [[:lua require("idris2.repl").evaluate()<CR>]], d(bufopts, "evaluate"))
+-- set("n", "<leader>ics", cmd .. ".case_split()<CR>", d(bufopts, "case split"))
+-- set("n", "<leader>imc", cmd .. ".make_case()<CR>", d(bufopts, "make case"))
+-- set("n", "<leader>iml", cmd .. ".make_lemma()<CR>", d(bufopts, "make lemma"))
+-- set("n", "<leader>iac", cmd .. ".add_clause()<CR>", d(bufopts, "add clause"))
+-- set("n", "<leader>ies", cmd .. ".expr_search()<CR>", d(bufopts, "search expression"))
+-- set("n", "<leader>igd", cmd .. ".generate_def()<CR>", d(bufopts, "generate definition"))
+-- set("n", "<leader>irh", cmd .. ".refine_hole()<CR>", d(bufopts, "refine hole"))
+-- set("n", "<leader>iin", cmd .. ".intro()<CR>", d(bufopts, "intro"))
