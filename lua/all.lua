3bc566567e7a435dc0859a011f323eafdaa3121e
LitFill <marrazzy54@gmail.com>
2025-02-19 18:36:57 +0700

    feat(all): add some feature

--- lua/all.lua
+++ lua/all.lua
@@ -1,28 +1,34 @@
 ---@diagnostic disable: undefined-global
 
+--- list of mini.nvim family ofplugins I want to be activated
+---@type string[]
 local enabled_modules = {
-    'git',
-    'diff',
-    'icons',
-    'statusline',
-    'starter',
-    'indentscope',
-    'files',
+    'ai',
+    'align',
+    'bracketed',
     'comment',
     'completion',
-    'notify',
-    'bracketed',
-    'pick',
+    'cursorword',
+    'diff',
+    'doc',
+    'extra',
+    'files',
+    'git',
+    'icons',
+    'indentscope',
     'jump',
     'jump2d',
-    'align',
-    'surround',
-    'ai',
-    'operators',
     'move',
+    'notify',
+    'operators',
     'pairs',
-    'trailspace',
+    'pick',
     'splitjoin',
+    'starter',
+    'statusline',
+    'surround',
+    'tabline',
+    'trailspace',
 }
 
 for _, module in ipairs(enabled_modules) do
@@ -105,7 +111,7 @@ add {
 require('mason').setup()
 local lsp = require 'lspconfig'
 lsp.lua_ls.setup {}
-lsp.hls.setup {}
+-- lsp.hls.setup {}
 
 add {
 	source   = 'nvim-treesitter/nvim-treesitter',
@@ -176,3 +182,98 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 -- }
 --
 -- require("pretty-fold").setup()
+
+add {
+    source = "mrcjkb/haskell-tools.nvim",
+    name   = "haskell-tools"
+}
+
+local function setup_haskell_keymaps()
+    local ht    = require('haskell-tools')
+    local bufnr = vim.api.nvim_get_current_buf()
+    local opt   = { buffer = bufnr }
+    local set   = vim.keymap.set
+
+    local nset = function(key, cmd, opts)
+        set('n', key, cmd, opts)
+    end
+
+    ---@param desc string
+    ---@return { buffer: number, desc: string}
+    local desc = function(desc)
+        opt.desc = desc
+        return opt
+    end
+
+    -- Leader-c mappings untuk Cabal
+    nset('<Leader>cb', '<CMD>15split | term cabal build<CR>', desc 'Cabal Build'    )
+    nset('<Leader>cr', '<CMD>15split | term cabal run<CR>',   desc 'Cabal Run'      )
+    nset('<Leader>ct', '<CMD>15split | term cabal test<CR>',  desc 'Cabal Test'     )
+    nset('<Leader>cc', '<CMD>edit *.cabal<CR>',               desc 'Open Cabal File')
+
+    -- Leader-h mappings untuk Haskell Tools
+    nset('<Leader>hh',  ht.hoogle.hoogle_signature, desc 'Hoogle Search'      )
+    nset('<Leader>hr',  ht.repl.toggle,             desc 'Toggle REPL'        )
+    nset('<Leader>ht',  ht.lsp.buf_hover,           desc 'Show Type Signature')
+    nset('<Leader>hca', ht.lsp.code_actions,        desc 'Code Actions'       )
+
+    nset('<Leader>hd',  ht.docs.buf.hover,          desc 'Show Documentation' )
+    -- nset('<Leader>hf', '<CMD>lua vim.lsp.buf.format()<CR>', desc 'Format File')
+end
+
+-- Aktifkan keymaps hanya untuk file Haskell
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { "haskell", "cabal", "*.hs", "*.cabal" },
+    callback = setup_haskell_keymaps,
+})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = "*",
+    callback = function()
+        vim.o.winbar = "%{%v:lua.GetShortPath()%}"
+    end
+})
+
+---@return string
+function GetShortPath()
+    local buf_path = vim.api.nvim_buf_get_name(0)
+    if buf_path == "" then
+        return "" -- Jika buffer kosong, tidak tampilkan apa-apa
+    end
+
+    local home         = vim.fn.expand("$HOME")
+    local project_home = home .. "/proyek"
+
+    -- Ganti bagian depan jika cocok
+    if buf_path:find("^" .. vim.pesc(project_home)) then
+        buf_path = buf_path:gsub(
+            "^" .. vim.pesc(project_home) .. "/",
+            ""
+        )
+    elseif buf_path:find("^" .. vim.pesc(home)) then
+        buf_path = buf_path:gsub(
+            "^" .. vim.pesc(home) .. "/",
+            ""
+        )
+    end
+
+    -- Format path menggunakan '->' sebagai separator
+    local display_path = buf_path:gsub("/", " -> ")
+
+    return "%#Normal#  LitFill :: " .. display_path
+end
+
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = "*",
+    once = true,
+    callback = function() vim.opt.scrolloff = 8 end
+})
+
+add {
+    source = "MeanderingProgrammer/render-markdown.nvim",
+    name   = "render-markdown",
+    depends = {
+        'nvim-treesitter/nvim-treesitter',
+        'echasnovski/mini.nvim'
+    },
+}
