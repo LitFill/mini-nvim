5942a809f507fceb07fd7215477223c17e334975
LitFill <litfill@DESKTOP-N7L07V4.>
2025-03-01 00:31:31 +0700

    style(lua/all): fix indent

--- lua/all.lua
+++ lua/all.lua
@@ -82,14 +82,14 @@ require('mini.clue').setup {
 
 local gen_loader = require('mini.snippets').gen_loader
 require('mini.snippets').setup {
-  snippets = {
-    -- Load custom file with global snippets first (adjust for Windows)
-    gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+    snippets = {
+      -- Load custom file with global snippets first (adjust for Windows)
+      gen_loader.from_file('~/.config/nvim/snippets/global.json'),
 
-    -- Load snippets based on current language by reading files from
-    -- "snippets/" subdirectories from 'runtimepath' directories.
-    gen_loader.from_lang(),
-  },
+      -- Load snippets based on current language by reading files from
+      -- "snippets/" subdirectories from 'runtimepath' directories.
+      gen_loader.from_lang(),
+    },
 }
 
 require "options"
