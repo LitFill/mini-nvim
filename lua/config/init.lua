72e16c4000fb9c4f5dfc116000b91ac203cec2ce
LitFill <marrazzy54@gmail.com>
2025-10-02 14:29:06 +0700

    aliasing require to import

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,41 +1,41 @@
-require "config.minis"
+local import = require
 
-require "core.options"
-require "core.keymaps"
-require "plugins"
-require "core.autocmds"
-require "utils"
+import "config.minis"
+
+import "core.options"
+import "core.keymaps"
+import "plugins"
+import "core.autocmds"
+import "utils"
 
 vim.cmd "colorscheme miniwinter"
 
 ----------------------------------------
---- Plugin configurations
+---      Plugin configurations       ---
 ----------------------------------------
-require  "config.treesitter"
-require  "config.lsp"
+import  "config.treesitter"
+import  "config.lsp"
 
-require  "origami"        .setup()
-require  "todo-comments"  .setup()
-require  "colorizer"      .setup()
-require  "sqlua"          .setup()
+import  "origami"        .setup()
+import  "todo-comments"  .setup()
+import  "colorizer"      .setup()
+import  "sqlua"          .setup()
 
-require  "stay-centered"  .setup
+import  "stay-centered"  .setup
 {
     skip_filetypes = {""},
 }
 
-require  "idris2"         .setup
+import  "idris2"         .setup
 {
-    client = {
-        hover = {
-            use_split         = true,    -- Persistent split instead of popups for hover
-            auto_resize_split = true,    -- Should resize split to use minimum space
-            with_history      = true,    -- Show history of hovers instead of only last
-            split_position    = "right", -- bottom, top, left or right
-            split_size        = "20%",   -- Size of persistent split, if used
-        },
-    },
+    client = { hover = {
+        use_split         = true,    -- Persistent split instead of popups for hover
+        auto_resize_split = true,    -- Should resize split to use minimum space
+        with_history      = true,    -- Show history of hovers instead of only last
+        split_position    = "right", -- bottom, top, left or right
+        split_size        = "20%",   -- Size of persistent split, if used
+    }},
 }
 
 -- ensure this ↓ is the last line
-require "config.neovide"
+import "config.neovide"
